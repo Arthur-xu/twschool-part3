baf3ac028197e0f4e9d1a30853222ad8533883b5
shuming-xu <shuming.xu@rea-group.com>
2019-03-16 19:07:13 +0800

    update:add antDesign in CreateSchool

--- src/pages/AddInstitution/CreateSchool.js
+++ src/pages/AddInstitution/CreateSchool.js
@@ -1,6 +1,13 @@
 import React from 'react';
+
 import CONSTANT from '../../constant/other.constant';
 import URL from '../../constant/http.constant';
+
+import { Typography, Form, Input, Button } from 'antd';
+const { Title } = Typography;
+const { TextArea } = Input;
+
+
 export default class CreateSchool extends React.Component {
     constructor(props) {
         super(props);
@@ -49,27 +56,43 @@ export default class CreateSchool extends React.Component {
     }
 
     render() {
+
+        const { formLayout } = this.state;
+        const formItemLayout = formLayout === 'horizontal' ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+        } : null;
+        const buttonItemLayout = formLayout === 'horizontal' ? {
+            wrapperCol: { span: 14, offset: 4 },
+        } : null;
         return (
             <div className="App">
-                <div ><button onClick={this.backToList}>返回</button></div>
-                <h3>创建训练营</h3>
-                <div>
-                    <div>
-                        标题：
-                <input type="text" value={this.state.title} onChange={this.handleTitleChange} />
-                    </div>
-                    <div>
-                        描述：
-                <textarea value={this.state.describe} onChange={this.handleTextareInput}></textarea>
+                <div><Button type="primary" onClick={this.backToList}>返回</Button></div>
+                <Title level={3}>创建训练营</Title>
+
+                <Form layout={formLayout}>
+                    <Form.Item
+                        label="标题："
+                        {...formItemLayout}
+                    >
+                        <Input value={this.state.title} onChange={this.handleTitleChange} placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item
+                        label="描述：¡"
+                        {...formItemLayout}
+                    >
+                        <TextArea value={this.state.describe} onChange={this.handleTextareInput} placeholder="Autosize height with minimum and maximum number of lines" autosize={{ minRows: 2, maxRows: 6 }} />
                         <span style={{ display: this.state.canInput ? 'none' : 'inline-block' }}>最多{CONSTANT.MAX_LENGTH}个字</span>
                         <span>{this.state.describe.length}/{CONSTANT.MAX_LENGTH}</span>
-                    </div>
-                </div>
-                <div>
-                    <button onClick={this.backToList}>取消</button>
-                    <button onClick={this.doSave}>保存</button>
-                </div>
-            </div>
+                    </Form.Item>
+                    <Form.Item {...buttonItemLayout}>
+                        <Button onClick={this.backToList} type="primary">取消</Button>
+                        <Button onClick={this.doSave} type="primary">保存</Button>
+                    </Form.Item>
+                </Form>
+
+
+            </div >
         );
     }
 }
