9ea1485d2ae1742a7eab3aa0a6aa36096803d7cf
Yang Liu <yang.liu1@thoughtworks.com>
2019-03-17 06:07:07 +0800

    Add some assets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Route from './router/index';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
-import MainLayout from './Layout';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+import './App.scss'
 import 'antd/dist/antd.css'
 
-const render = (Component) => {
-    ReactDOM.render(<MainLayout>
-        <Component />
-      </MainLayout>,
-    document.getElementById('root'));
-}
-render(Route);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'))
