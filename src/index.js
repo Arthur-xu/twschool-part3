96b7533e1b50f55be4a800806ee015cb253ff5d0
Yang Liu <yang.liu1@thoughtworks.com>
2019-03-17 14:06:26 +0800

    Fix some issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
-import CreateSchool from "./pages/AddInstitution/CreateSchool";
 
 import './App.scss'
 import 'antd/dist/antd.css'
--- src/pages/AddInstitution/CreateSchool.js
+++ src/pages/AddInstitution/CreateSchool.js
@@ -6,109 +6,115 @@ import URL from '../../constant/http.constant'
 import './CreateSchool.scss'
 import commonFun from '../../common/commonFun'
 
-import { Form, Input, message } from 'antd';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
-const { TextArea } = Input;
+import { Form, Input, message } from 'antd'
+import { Link } from 'react-router-dom'
+const { TextArea } = Input
 
 export default class CreateSchool extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: '',
-            describe: '',
-            canInput: true,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      describe: '',
+      canInput: true
+    }
+  }
+
+  backToList = () => {
+    this.props.history.push('/InstitutionList')
+  }
+
+  doSave = () => {
+    if (!this.state.title || !this.state.describe) {
+      return message.warning('标题和描述必输')
     }
 
-    backToList = () => {
-        this.props.history.push('/InstitutionList');
+    if (this.state.title.length > 20) {
+      return message.warning('标题长度小于20')
     }
 
-    doSave = () => {
-        if (!this.state.title || !this.state.describe) {
-            return message.warning('标题和描述必输')
-        }
-
-        if (this.state.title.length > 20) {
-            return message.warning('标题长度小于20')
-        }
-
-
-        const params = {
-            title: this.state.title,
-            content: this.state.describe
-        };
-
-        const sucFun = (response) => {
-            console.log('should go to list');
-            message.success('This is a message of success');
-            setTimeout(() => {
-                this.backToList();
-            }, 500);
-        }
-
-        const failFun = (error) => {
-            console.log(error);
-            message.error('This is a message of error');
-        }
-
-        commonFun.createRequest({ URL: URL.create, params, method: 'POST', sucFun, failFun });
+    const params = {
+      title: this.state.title,
+      content: this.state.describe
     }
 
-    handleTextareInput = event => {
-        this.setState({
-            canInput: event.target.value.length < CONSTANT.MAX_LENGTH
-        })
-        if (this.state.canInput) {
-            this.setState({
-                describe: event.target.value
-            })
-        }
+    const sucFun = response => {
+      console.log('should go to list')
+      message.success('This is a message of success')
+      setTimeout(() => {
+        this.backToList()
+      }, 500)
     }
 
-    handleTitleChange = event => {
-        this.setState({
-            title: event.target.value
-        })
+    const failFun = error => {
+      console.log(error)
+      message.error('This is a message of error')
     }
 
-    render() {
-        return (
-            <div className="create-school">
+    commonFun.createRequest({
+      URL: URL.create,
+      params,
+      method: 'POST',
+      sucFun,
+      failFun
+    })
+  }
 
-                <div className="back-button">
-                    <Link to="/InstitutionList">返回</Link>
-                </div>
+  handleTextareInput = event => {
+    this.setState({
+      canInput: event.target.value.length < CONSTANT.MAX_LENGTH
+    })
+    if (this.state.canInput) {
+      this.setState({
+        describe: event.target.value
+      })
+    }
+  }
 
-                <h3 className="title">创建训练营</h3>
-                <Form>
-                    <Form.Item label="标题">
-                        <Input value={this.state.title} onChange={this.handleTitleChange} />
-                    </Form.Item>
-                    <Form.Item label="描述">
-                        <TextArea
-                            value={this.state.describe}
-                            onChange={this.handleTextareInput}
-                        />
-                        <span
-                            style={{ display: this.state.canInput ? 'none' : 'inline-block' }}
-                        >
-                            最多{CONSTANT.MAX_LENGTH}个字
+  handleTitleChange = event => {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="create-school">
+        <div className="back-button">
+          <Link to="/InstitutionList">返回</Link>
+        </div>
+
+        <h3 className="title">创建训练营</h3>
+        <Form>
+          <Form.Item label="标题">
+            <Input value={this.state.title} onChange={this.handleTitleChange} />
+          </Form.Item>
+          <Form.Item label="描述">
+            <TextArea
+              value={this.state.describe}
+              onChange={this.handleTextareInput}
+            />
+            <span
+              style={{ display: this.state.canInput ? 'none' : 'inline-block' }}
+            >
+              最多{CONSTANT.MAX_LENGTH}个字
             </span>
-                        <span>
-                            {this.state.describe.length}/{CONSTANT.MAX_LENGTH}
-                        </span>
-                    </Form.Item>
-                </Form>
-                <div className="footer">
-                    <div className="button-container">
-                        <Link to="/InstitutionList" className="cancel-button">
-                            取消
-                        </Link>
-                        <div className="submit-button" onClick={this.doSave}>保存</div>
-                    </div>
-                </div>
+            <span>
+              {this.state.describe.length}/{CONSTANT.MAX_LENGTH}
+            </span>
+          </Form.Item>
+        </Form>
+        <div className="footer">
+          <div className="button-container">
+            <Link to="/InstitutionList" className="cancel-button">
+              取消
+            </Link>
+            <div className="submit-button" onClick={this.doSave}>
+              保存
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
